767c47bcf4129850ad6055b9e4115c25bdcd2ab5
isaac <isaacongera717@gmail.com>
2022-11-23 15:47:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + 'Grace Hopper,!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + 'GRACE HOPPER,!';
+
+const shortGreeting = 'Welcome,  ${G.slice(0, 1)}!';
+
